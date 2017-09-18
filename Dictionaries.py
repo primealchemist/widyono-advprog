58f7f72f7e0d56469cf1452cc36392d6a22f852b
primealchemist <31862091+primealchemist@users.noreply.github.com>
2017-09-18 13:41:32 -0400

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,15 @@
+eng2fr = {}
+
+eng2fr['one'] = 'un'
+eng2fr['two'] = 'deux'
+eng2fr['three'] = 'trois'
+
+print eng2fr
+
+print eng2fr['one']
+print eng2fr['two']
+print eng2fr['three']
+
+
+f = {}
+a = {}
